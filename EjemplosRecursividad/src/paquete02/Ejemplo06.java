b846d1056b4fc7486a4da872a487c9b22f4a7ad3
Erick <estoledo1@utpl.edu.ec>
2024-01-11 17:52:58 -0500

    ejemplos

--- EjemplosRecursividad/src/paquete02/Ejemplo06.java
+++ EjemplosRecursividad/src/paquete02/Ejemplo06.java
@@ -19,21 +19,21 @@ public class Ejemplo06 {
         for (int i = 0; i < valor1.length; i++) {
             System.out.printf("El valor es: %d\n", 
                     obtenerSuma(valor1[i], valor2[i]));
-        }
+        }// se envia los valores direto de los arreglos 
         
         for (int i = 0; i < valor1.length; i++) {
             int num1 = valor1[i];
             int num2 = valor2[i];
             System.out.printf("El valor es: %d\n", 
                     obtenerSuma(num1, num2));
-        }
+        }// se toma los valores del arreglo para devilver de una variable int
         
         for (int i = 0; i < valor1.length; i++) {
             int num1 = valor1[i];
             int num2 = valor2[i];
             int suma = obtenerSuma(num1, num2); 
             System.out.printf("El valor es: %d\n", suma);
-        }
+        }// se toma la funcion dentro de un valor entero apra poder presentarlo
         
         
 
--- EjemplosRecursividad/src/paquete02/Ejemplo08.java
+++ EjemplosRecursividad/src/paquete02/Ejemplo08.java
@@ -4,13 +4,28 @@
  */
 package paquete02;
 
+import java.util.Scanner;
+
 public class Ejemplo08 {
 
     public static void main(String[] args) {
         // Ingresar por teclado el tamaño de un arreglo
         // Ingresar por teclado los valores enteros del arreglo
         // Sumar los valores del arreglo, mediante la función misterio
-        
+       
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("define el tamanio del arreglo");
+        int n = entrada.nextInt();
+        int [] arreglo = new int[n];
+        for (int i = 0; i < arreglo.length; i++) {
+            System.out.println("ingrese los datos del arreglo");
+            arreglo[i]= entrada.nextInt();
+            
+        }
+         for (int i = 0; i < arreglo.length; i++) {
+             misterio(arreglo, n);
+        }
+         System.out.println(misterio(arreglo, arreglo.length));
     }
 
     public static int misterio(int[] arreglo, int tamanio) {
